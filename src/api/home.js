4dcb86420b058c10c229b35b0243bd111cdf1bba
PrincipleFirst <364753362@qq.com>
2017-07-14 23:17:45 +0800

    Delete home.js

--- src/api/home.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export function getProjects() {
-  const url = '/api/getProjects'
-  return axios.get(url).then((res) => {
-    return Promise.resolve(res.data)
-  })
-}
